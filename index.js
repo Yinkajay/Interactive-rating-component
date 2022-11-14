70d3d07bf36594164d74148ea38ff04f7fe995cb
Adeyinka Junaid <yinkajunn@gmail.com>
2022-11-14 20:01:31 +0100

    Fix submit functionality

--- index.js
+++ index.js
@@ -3,32 +3,35 @@ const pageBody = document.querySelector('body');
 const submitBtn = document.getElementById('submit-btn')
 const beforeSubmission = document.querySelector('.pre-sub')
 const afterSubmission = document.querySelector('.post-sub')
+const answerSpan = document.querySelector('.answer')
 let answer = ''
 
 
-console.log(ratingOptions);
+// console.log(ratingOptions);
 
 
 ratingOptions.forEach(element => {
     element.addEventListener('click', () => {
-        element.classList.toggle('clicked')
+        ratingOptions.forEach((element) => { element.classList.remove('clicked') })
         answer = element.innerHTML
         console.log(answer);
+        element.classList.toggle('clicked')
     })
 });
 
 submitBtn.onclick = (event) => {
     event.preventDefault()
     console.log('value submitted');
-    toggleContent(1)
+    answerSpan.innerHTML = `${answer}`
+    toggleContent()
 }
 
 function toggleContent() {
-    if(answer.trim() === ''){
+    if (answer.trim() === '') {
         beforeSubmission.style.display = 'block'
         afterSubmission.style.display = 'none'
-    }else{
+    } else {
         beforeSubmission.style.display = 'none'
         afterSubmission.style.display = 'block'
     }
-}
+}   
